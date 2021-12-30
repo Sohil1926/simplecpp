89fa947b8975e9f1210bca72b6b93c31f6723521
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2021-12-30 17:06:17 -0500

    Update leap_year.cpp

--- leap_year.cpp
+++ leap_year.cpp
@@ -12,14 +12,10 @@ int main() {
         count++;
     }
 
-    if (count == 4 && year % 4 == 0) {
-        cout << "Leap year! \n";
+    if (count == 4 && (year % 4 == 0 || year % 400 == 0)) {
+        cout << "Leap year! 111\n";
     }
-
     else if (year % 100 == 0 && year % 400 != 0) {
         cout << "Not a leap year! \n";
-    }
-    else if (year % 400 == 0) {
-        cout << "Leap year! \n";
     }
 }
