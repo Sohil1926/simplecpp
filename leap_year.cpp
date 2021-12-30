b022ef17dd16fda25e9f08bcfd2a5377b90602d3
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2021-12-30 16:36:25 -0500

    Add files via upload

---
+++ leap_year.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int count = 0, year;
+
+    cout << "Enter a year: ";
+    cin >> year;
+
+    while (year > 0) {
+        year = year / 10;
+        count++;
+    }
+
+    if (count == 4 && year % 4 == 0) {
+        cout << "Leap year! \n";
+    }
+
+    else if (year % 100 == 0 && year % 400 != 0) {
+        cout << "Not a leap year! \n";
+    }
+    else if (year % 400 == 0) {
+        cout << "Leap year! \n";
+    }
+}
