3c8fe904575831ffbd6e6f875b812b00b5182d51
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2021-12-31 21:39:20 -0500

    Add files via upload

---
+++ 99bottles.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	
+	for (int i = 99; i > 0; i--) {
+		cout << i << " bottles of pop on the wall. Take one down and pass it around. " << i - 1 << " bottles of pop on the wall.🥤\n\n";
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
