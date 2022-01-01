fe3680cf9c8d74ba0c46589799fc888ee47f2707
Sohil Athare <61813964+Sohil1926@users.noreply.github.com>
2021-12-31 23:52:18 -0500

    Update leap_year.cpp

--- leap_year.cpp
+++ leap_year.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
+
 using namespace std;
 
 int main() {
-    int count = 0, year;
+  int count = 0, year;
 
-    cout << "Enter a year: ";
-    cin >> year;
+  cout << "Enter a year: ";
+  cin >> year;
 
-    while (year > 0) {
-        year = year / 10;
-        count++;
-    }
-    if (count == 4 && (year % 4 == 0 || year % 400 == 0)) {
-        cout << "Leap year! 111\n";
-    }
-    else if (year % 100 == 0 && year % 400 != 0) {
-        cout << "Not a leap year! \n";
-    }
+  while (year > 0) {
+    year = year / 10;
+    count++;
+  }
+  if (count == 4 && (year % 4 == 0 || year % 400 == 0)) {
+    cout << "Leap year! 111\n";
+  } else if (year % 100 == 0 && year % 400 != 0) {
+    cout << "Not a leap year! \n";
+  }
 }
